b3cd388f91eca2443db3c043fe966286cfc81319
JJaros <yareky@protonmail.com>
2024-06-01 23:53:43 +0200

    Add readme Improve RepoMemoryCache

---
+++ README.md
@@ -0,0 +1,4 @@
+1. No visual errors handling (out of scope) but the code is ready for implementation (passing data
+   as Status<> class)
+2. Caching implemented as simple memory cache. SqlLite/RoomDB implementation available as a paid
+   DLC - Sry, my rate is to low for the scope of this project!
--- data/src/main/java/com/example/data/repositoryImpl/RepoMemoryCache.kt
+++ data/src/main/java/com/example/data/repositoryImpl/RepoMemoryCache.kt
@@ -11,42 +11,51 @@ class RepoMemoryCache(
     private val queryMap: MutableMap<String, List<Long>> = mutableMapOf()
 ) : IRepoCache {
 
-    override suspend fun putImages(hits: List<Hit>) =
-        hitsMap.putAll(hits.associateBy { it.id })
+    override suspend fun putImages(hits: List<Hit>) {
+        synchronized(this) {
+            hitsMap.putAll(hits.associateBy { it.id })
+        }
+    }
 
     override suspend fun cacheQuery(
         query: String,
         searchImagesResponse: SearchImagesResponse
     ) {
-        if (searchImagesResponse.hits.isNotEmpty()) {
-            putImages(searchImagesResponse.hits)
-            queryMap[query] = searchImagesResponse.hits.map { it.id }
+        synchronized(this) {
+            if (searchImagesResponse.hits.isNotEmpty()) {
+                hitsMap.putAll(searchImagesResponse.hits.associateBy { it.id })
+                queryMap[query] = searchImagesResponse.hits.map { it.id }
+            }
         }
     }
 
     override suspend fun searchImages(query: String): Result<SearchImagesResponse> {
-        return queryMap[query]?.let { ids ->
-            ids.mapNotNull { hitsMap[it] }.let { hits ->
-                if (hits.isNotEmpty()) {
-                    SearchImagesResponse(hits.size, hits.size, hits)
-                } else {
-                    null
+        synchronized(this) {
+            return queryMap[query]?.let { ids ->
+                ids.mapNotNull { hitsMap[it] }.let { hits ->
+                    if (hits.isNotEmpty()) {
+                        SearchImagesResponse(hits.size, hits.size, hits)
+                    } else {
+                        null
+                    }
                 }
-            }
-        }?.let { Status.SUCCESS(it) }
-            ?: Status.ERROR(
-                NO_SUCH_ELEMENT_EXCEPTION
-            )
+            }?.let { Status.SUCCESS(it) }
+                ?: Status.ERROR(
+                    NO_SUCH_ELEMENT_EXCEPTION
+                )
+        }
     }
 
-    override suspend fun searchImage(imageId: Long): Result<Hit> =
-        hitsMap[imageId]?.let { Status.SUCCESS(it) }
-            ?: Status.ERROR(
-                NO_SUCH_ELEMENT_EXCEPTION
-            )
+    override suspend fun searchImage(imageId: Long): Result<Hit> {
+        synchronized(this) {
+            return hitsMap[imageId]?.let { Status.SUCCESS(it) }
+                ?: Status.ERROR(
+                    NO_SUCH_ELEMENT_EXCEPTION
+                )
+        }
+    }
 
     private companion object {
         val NO_SUCH_ELEMENT_EXCEPTION = NoSuchElementException()
-        val EMPTY_RESPONSE = SearchImagesResponse(0, 0, emptyList())
     }
 }
